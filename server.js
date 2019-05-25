705102ee679e51e780c6a3e7ddedab95f0ecfcf9
stevekeol <stevekeol@users.noreply.github.com>
2019-05-25 22:50:25 +0800

    增加功能：条件停止，字段投射

--- server.js
+++ server.js
@@ -1,209 +1,91 @@
-var http = require("http"),
- url = require("url"),
- superagent = require("superagent"), //客户端请求代理模块
- cheerio = require("cheerio"), //Node.js的抓取页面模块
- async = require("async"),
- eventproxy = require('eventproxy'); //解决回调嵌套，让串行等待变成并行等待
+// const http = require("http");
+// const url = require("url");
+// const cheerio = require("cheerio"); //Node.js的抓取页面模块
+// const async = require("async");
+// const eventproxy = require('eventproxy'); //解决回调嵌套，让串行等待变成并行等待
 
-var ep = new eventproxy();
+// const EVENTPROXY = new eventproxy();
 
-var catchFirstUrl = 'http://www.cnblogs.com/',	//入口页面
-	deleteRepeat = {},	//去重哈希数组
-	urlsArray = [],	//存放爬取网址
-	catchData = [],	//存放爬取数据
-	pageUrls = [],	//存放收集文章页面网站
-	pageNum = 2,	//要爬取文章的页数
-	startDate = new Date(),	//开始时间
-	endDate = false;	//结束时间
+// const test = 'https://h5.ele.me/restapi/member/v2/users/223030102/location?longitude=120.064087&latitude=30.282619';
 
-for(var i=1 ; i<= pageNum ; i++){
-	pageUrls.push('http://www.cnblogs.com/?CategoryId=808&CategoryType=%22SiteHome%22&ItemListActionName=%22PostList%22&PageIndex='+ i +'&ParentCategoryId=0');
-}
+// const xhrUrl_init = 'https://h5.ele.me/restapi/shopping/v3/restaurants?latitude=30.282619&longitude=120.064087&offset=0&limit=8&extras[]=activities&extras[]=tags&extra_filters=home&rank_id=&terminal=h5';
 
-// 抓取昵称、入园年龄、粉丝数、关注数
-function personInfo(url){
-	var infoArray = {};
-	superagent.get(url)
-		.end(function(err,ares){
-			if (err) {
-	      console.log(err);
-	      return;
-	    }
+// const test_0 = 'https://www.itjuzi.com/login';
 
-	    var $ = cheerio.load(ares.text),
-				info = $('#profile_block a'),
-  	  	len = info.length,
-  	  	age = "",
-  	  	flag = false,
-  	  	curDate = new Date();
+// let xhrUrl = `https://h5.ele.me/restapi/shopping/v3/restaurants?latitude=30.282619&longitude=120.064087&offset=${offset}&limit=8&extras[]=activities&extras[]=tags&extra_filters=home&rank_id=${rank_id}&terminal=h5`;
 
-  	  // 小概率异常抛错	
-  	  try{
-  	  	age = "20"+(info.eq(1).attr('title').split('20')[1]);
-  	  }
-  	  catch(err){
-  	  	console.log(err);
-  	  	age = "2012-11-06";
-  	  }	
 
-    	infoArray.name = info.eq(0).text();
-    	infoArray.age = parseInt((new Date() - new Date(age))/1000/60/60/24);
-	    
-	    if(len == 4){
-	 	    infoArray.fans = info.eq(2).text();
-	    	infoArray.focus = info.eq(3).text();	
-	    }else if(len == 5){// 博客园推荐博客
-	 	    infoArray.fans = info.eq(3).text();
-	    	infoArray.focus = info.eq(4).text();	
-	    }
-	    //console.log('用户信息:'+JSON.stringify(infoArray));
-	    catchData.push(infoArray);
+const superagent = require("superagent"); //客户端请求代理模块
+const async = require("async");
+
+let	offset = 0;
+let	rank_id = '';
+let	has_next = true;
+let	limit = 5;
+let	index = 0;
+let	url = `https://h5.ele.me/restapi/shopping/v3/restaurants?latitude=30.282619&longitude=120.064087&offset=${offset}&limit=8&extras[]=activities&extras[]=tags&extra_filters=home&rank_id=${rank_id}&terminal=h5`;
+
+let start = function(offset, rank_id) {
+	let getStoreAllInfo = function(callback) {
+		superagent
+	    .get(url)
+	    .set('cookie', 'ubt_ssid=letbe83ed2dgw1ubh26xj4aoltlzhxx4_2019-05-22; _utrace=10da00394d52c6cf5dda8e0db2637e1d_2019-05-22; perf_ssid=p133tnt71er6h4bq71ylxhk12w6yqjsd_2019-05-22; cna=DUtqFSf+OyYCATy/M2IUrsNr; _bl_uid=91jU5v3OzeF86mowj7IR3vay6gFv; track_id=1558530274|515fc23c0beb6f1e06927ec367dfdb2e67521443236a8e7ef5|2b3c075a11ca151673d2757abf23dbcd; UTUSER=223030102; tzyy=cc1c26b6992fbc80ad70dc9c7e43c4ac; USERID=223030102; SID=utcIBo9hys04tpgF619JEW1Ht8YI1Ce6UMrg; ZDS=1.0|1558688062|k2Cw5hrBoSF5h1bzz5IGf2ClbaFSKsfgbUtW4GR/qA3qurTczV5LN9iPfDLRb6Rl; isg=BKGhjU9wWZcbjPVhbghCXgAosG2xRB_HiD2PXAN2lagHask8TJ7hElOsyNjJoq14')
+	    .set('referer', 'https://h5.ele.me/')
+	    .set('user-agent', 'Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Mobile/15A372 Safari/604.1')
+	    .set('x-uab', '118#ZVWZz2sMZu/oXZCfBHRIZYquZYT4zHWzagC2Voq4mMXHoUZTyHRVPgZuusqhzeWZZZZZXoqVzeAuZZZh0HWWGcb/ZzqTqhZzZgeCc5qVzHZzZeZTVHRVZZZuZwqhzeWzZZZuXTbVzHZzZeZhTgKfgH2Z5Nh6DwmYtHFUpoq4cAIJ+llc/guWQsu2Us41manDSp8JzBnss3ybyCbBub1Lz2wqTA5ZvtM7AY5iQ1uTugZCmrDtKHZzhCAYu+t5RgZTlJTRSmiVZFVxzWaEYrxGqLmRx41C2dYob1PLoF1IV0B47DhVSv8bVcY65NhWeAWBgeo4Gf2fFCcgZrIpJ9EG/tjt7G5jpZwC5GQwIYFOfH0NrLhg+DML4+mFz8vpwAAzIDt+l3PI2bJe0VB1rTOszhOeTweJ0VlZYb0yAhmKHzoknkGQfJH6vLYh1F+fyGfexUZ8Hy19lQWQlmgUSfOfAF1NR/jzEmm6qt6b4ycVdtdNtrdaSn2d4lGJVfLS+aHfIsBpIZzEI9uqU1lmboetZGxrTOchqvK/Li2PZs6LAHNgq8x9NsZkXZtWU7sUD8hBLFnzhIkGm33if/D2+mGBbgEwUhQrwG6fWSmsk/oLZM5RnUa2Hmgbcr6Ky1UkCI+ZrZUlYSPRkvgvlVVWYbOXD9cOccUfRsbM9dtxh8dF47Hx54ow0S2fwzfP5IfqxNdMtVfa3FpoOlvtCATCrcCu/Nknf0R9MIMQJAqqikU3b+GW8Qy+dicNIeGjVcYFSTA=')
+	    .end(function(err, res) {
+	    	// console.log(res.body);
+	      callback(null, res.body);
+	    });	
+	}
+
+	let getStoreCriticalInfo = function(storeAllInfos, callback) {
+		let storeList = [...storeAllInfos.items];
+		storeList.forEach(function(store, index, arr) {
+			let storeInfo = {
+				name: '', //商家名称
+				id: '', //商家id
+				phone: '', //商家电话
+				address: '', //商家地址
+				scheme: '', //商家主页
+				flavors: [], //品类
+				is_new: false, //是否新商家
+				latitude: '', //商家纬度
+				longitude: '', //商家经度
+				rating: '', //商家评分
+				recent_order_num: '', //月销量
+				order_lead_time: '' //平均配送时长
+			};
+			console.log(`\n第${offset+index}家店铺：`);
+			storeInfo.name = store.restaurant.name;
+			storeInfo.id = store.restaurant.id;
+			storeInfo.phone = store.restaurant.phone;
+			storeInfo.address = store.restaurant.address;
+			storeInfo.scheme = store.restaurant.scheme;
+			storeInfo.flavors = store.restaurant.flavors;
+			storeInfo.is_new = store.restaurant.is_new;
+			storeInfo.latitude = store.restaurant.latitude;
+			storeInfo.longitude = store.restaurant.longitude;
+			storeInfo.rating = store.restaurant.rating;
+			storeInfo.recent_order_num = store.restaurant.recent_order_num;
+			storeInfo.order_lead_time = store.restaurant.order_lead_time;	
+			console.log(storeInfo);
+		});
+		callback(null, storeAllInfos);
+	}
+
+	async.waterfall([getStoreAllInfo, getStoreCriticalInfo], function(err, result) {
+		has_next = result.has_next;
+		offset = offset + 8;
+		rank_id = result.meta.rank_id;
+		url = `https://h5.ele.me/restapi/shopping/v3/restaurants?latitude=30.282619&longitude=120.064087&offset=${offset}&limit=8&extras[]=activities&extras[]=tags&extra_filters=home&rank_id=${rank_id}&terminal=h5`;
+		index = index + 1;
+		if(has_next && index < 1) {
+			start(offset, rank_id);
+		} else {
+			console.log(`\n已经停止于： ${new Date()}, 共执行了${index}个循环！\n`)
+		}
 	});
 }
 
-// 判断作者是否重复
-function isRepeat(authorName){
-	if(deleteRepeat[authorName] == undefined){
-		deleteRepeat[authorName] = 1;
-		return 0;
-	}else if(deleteRepeat[authorName] == 1){
-		return 1;
-	}
-}
-
-// 主start程序
-function start(){
-	function onRequest(req, res){
-		// 设置字符编码(去掉中文会乱码)
-		res.writeHead(200, {'Content-Type': 'text/html;charset=utf-8'});
-		// 当所有 'BlogArticleHtml' 事件（即监听到pageUrls.length*20次该事件后）完成后的回调触发下面事件。
-		// articleUrls是个数组，每一项是该事件携带的数据:articleUrl。
-		ep.after('BlogArticleHtml',pageUrls.length*20, function(articleUrls){
-			
-			// 获取 BlogPageUrl 页面内所有文章链接
-			for(var i = 0 ; i < articleUrls.length ; i++){
-      	res.write(articleUrls[i] +'<br/>');  
-      }    
-     	console.log('articleUrls.length is '+ articleUrls.length +',content is :'+articleUrls);
-
-     	//控制并发数
-     	var curCount = 0;
-     	var reptileMove = function(url,callback){
-     		//延迟毫秒数
-     		var delay = parseInt((Math.random() * 30000000) % 1000, 10);
-			  curCount++;
-			  console.log('现在的并发数是', curCount, '，正在抓取的是', url, '，耗时' + delay + '毫秒');  
-		  	
-		  	superagent.get(url)
-		  		.end(function(err,sres){
-			  		// 常规的错误处理
-			      if (err) {
-			        console.log(err);
-			        return;
-			      }		  	
-
-			      //sres.text 里面存储着请求返回的 html 内容
-			      var $ = cheerio.load(sres.text);
-			      //收集数据
-			      //1、收集用户个人信息，昵称、园龄、粉丝、关注
-						//var currentBlogApp = $('script').eq(1).text().split(',')[0].split('=')[1].trim().replace(/'/g,""),
-						var currentBlogApp = url.split('/p/')[0].split('/')[3],	
-							requestId = url.split('/p/')[1].split('.')[0];
-
-						res.write('currentBlogApp is '+ currentBlogApp + ' , ' + 'requestId id is ' + requestId +'<br/>'); 
-						console.log('currentBlogApp is '+ currentBlogApp + '\n' + 'requestId id is ' + requestId); 
-
-						res.write('the article title is :'+$('title').text() +'<br/>');
-
-						var flag = 	isRepeat(currentBlogApp);
-						
-						if(!flag){
-							var appUrl = "http://www.cnblogs.com/mvc/blog/news.aspx?blogApp="+ currentBlogApp;
-							personInfo(appUrl);
-						}
-		  		});
-
-		  	setTimeout(function() {
-			    curCount--;
-			    callback(null,url +'Call back content');
-			  }, delay);		
-     	};
-
-	    // 使用async控制异步抓取 	
-	    // mapLimit(arr, limit, iterator, [callback])
-	    // 异步回调
-      async.mapLimit(articleUrls, 5, 
-      	function (url, callback) {
-			  	reptileMove(url, callback);
-				},
-				function (err,result) {
-					endDate = new Date();
-
-				  console.log('final:');
-				  console.log(result);
-				  console.log(catchData);
-				  var len = catchData.length,
-				  	aveAge = 0,
-				  	aveFans = 0,
-				  	aveFocus = 0;
-
-				  for(var i=0 ; i<len ; i++){
-				  	var eachDate = JSON.stringify(catchData[i]),
-				  		eachDateJson = catchData[i];
-
-				  	// 小几率取不到值则赋默认值	
-				  	eachDateJsonFans = eachDateJson.fans || 110;
-				  	eachDateJsonFocus = eachDateJson.focus || 11;
-				  		
-				  	aveAge += parseInt(eachDateJson.age);
-				  	aveFans += parseInt(eachDateJsonFans);
-				  	aveFocus += parseInt(eachDateJsonFocus);
-				  	res.write(eachDate +'<br/>'); 
-				  }
-
-				  //统计结果
-				  res.write('<br/>');
-				  res.write('<br/>');
-				  res.write('/**<br/>');
-				  res.write(' * 爬虫统计结果<br/>');
-				  res.write('**/<br/>');
-				  res.write('1、爬虫开始时间：'+ startDate +'<br/>');
-				  res.write('2、爬虫结束时间：'+ endDate +'<br/>');
-				  res.write('3、耗时：'+ (endDate - startDate) +'ms' +' --> '+ (Math.round((endDate - startDate)/1000/60*100)/100) +'min <br/>');
-				  res.write('4、爬虫遍历的文章数目：'+ pageNum*20 +'<br/>');
-				  res.write('5、作者人数：'+ len +'<br/>');
-				  res.write('6、作者入园平均天数：'+ Math.round(aveAge/len*100)/100 +'<br/>');
-				  res.write('7、作者人均粉丝数：'+ Math.round(aveFans/len*100)/100 +'<br/>');
-				  res.write('8、作者人均关注数：'+ Math.round(aveFocus/len*100)/100 +'<br/>');
-				});
-		});
-		
-		// 轮询 所有文章列表页
-		pageUrls.forEach(function(pageUrl){
-			superagent.get(pageUrl)
-				.end(function(err,pres){
-					console.log('fetch ' + pageUrl + ' successful');
-					res.write('fetch ' + pageUrl + ' successful<br/>');
-					// 常规的错误处理
-		      if (err) {
-		        console.log(err);
-		   		}
-		      // pres.text 里面存储着请求返回的 html 内容，将它传给 cheerio.load 之后
-		      // 就可以得到一个实现了 jquery 接口的变量，我们习惯性地将它命名为 `$`
-		      // 剩下就都是 jquery 的内容了
-		      var $ = cheerio.load(pres.text);
-		      var curPageUrls = $('.titlelnk');
-		      for(var i = 0 ; i < curPageUrls.length ; i++){
-		      	var articleUrl = curPageUrls.eq(i).attr('href');
-		      	urlsArray.push(articleUrl);
-		      	// 相当于一个计数器
-		      	ep.emit('BlogArticleHtml', articleUrl);
-		      }
-			})
-		})
-	}
-
-	http.createServer(onRequest).listen(3000);
-}
-
-exports.start= start;
+start(offset, rank_id);
